053e99db18414919886a4baf9562629964b6639b
alex <alex.scheduling@gmail.com>
2024-01-01 21:30:05 +1000

    remove bug code from line numbers

--- lua/line_numbers.lua
+++ lua/line_numbers.lua
@@ -1,16 +1,5 @@
 local numtogGrp = vim.api.nvim_create_augroup("NumberToggle", { clear = true })
 vim.api.nvim_create_autocmd(
-  { "BufEnter", "InsertLeave", "FocusGained"},
-  { pattern = "*",
-    --command = "set relativenumber",  -- if callback, vim.api.nvim_buf_set_option(0,relativenumber,true) ?
-    callback = function()
-      vim.api.nvim_buf_set_option(0, 'relativenumber', true)
-    end,
-    group = numtogGrp,
-    desc = "Turn on relative line numbering when the buffer is entered.",
-  }
-)
-vim.api.nvim_create_autocmd(
   { "BufLeave", "InsertEnter", "FocusLost"},
   { pattern = "*",
     --command = "set norelativenumber",
